55b7457e48cff9cef1e973332e12753b6046932d
Ebhuthi Venkata Sai Kr <90022898+venkat21krishna@users.noreply.github.com>
2024-07-01 23:24:02 +0530

    Update Biomedical.sql

--- Biomedical.sql
+++ Biomedical.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 28, 2019 at 06:32 PM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.1.18
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,20 +6,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `userauth`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `app`
---
 
 CREATE TABLE `app` (
   `id` int(11) NOT NULL,
@@ -43,35 +22,25 @@ CREATE TABLE `app` (
 --
 
 INSERT INTO `app` (`id`, `name`, `email`, `date`, `time`, `description`, `regtime`) VALUES
-(16, 'Soumyadip Chowdhury1', 'testUser@gmail.com', '6/20/2019', '10:00pm', 'Fever', '2019-06-08 12:22:26'),
-(17, 'Soumyadip Chowdhury2', 'testUser@gmail.com', '6/19/2019', '11:00pm', 'Fever', '2019-06-08 12:22:08'),
-(18, 'Soumyadip Chowdhury3', 'hello@gmail.com', '6/4/2019', '12:30am', 'Cold', '2019-06-08 13:04:17'),
-(19, 'Soumyadip Chowdhury4', 'abc@teamcg.com', '6/5/2019', '12:30am', 'Fever', '2019-06-14 11:40:45'),
-(21, 'Soumyadip Chowdhury5', 'testUser@gmail.com', '7/3/2019', '3:30am', 'Fever', '2019-07-03 08:36:17');
+(6, 'venkat', 'venkat@gmail.com', '1/20/2024', '11:00pm', 'Cold', '2024-07-11 10:12:24'),
+(7, 'sai', 'sai@gmail.com', '5/19/2024', '10:00pm', 'Fever', '2024-09-12 11:02:18'),
+(8, 'krishna', 'krishna@gmail.com', '9/4/2024', '01:30am', 'Cough', '2024-06-10 12:14:16'),
+(9, 'ebhuthi', 'ebhuthi@gmail.com', '5/5/2024', '12:40am', 'Fever', '2024-05-08 09:00:45'),
+(1, 'Sreenivasulu', 'sreenivasulu@gmail.com', '8/3/2024', '6:30pm', 'Headache', '2024-01-09 03:26:17');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `hibernate_sequence`
---
 
 CREATE TABLE `hibernate_sequence` (
-  `next_val` bigint(20) DEFAULT NULL
+  `next_value` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `hibernate_sequence`
---
 
-INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(22),
-(22);
 
--- --------------------------------------------------------
+INSERT INTO `hibernate_sequence` (`next_value`) VALUES
+(25),
+(25);
 
---
--- Table structure for table `user`
---
+
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -91,38 +60,25 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `confirmation_token`, `username`, `enabled`, `first_name`, `gender`, `last_name`, `password`, `authority`, `lastseen`) VALUES
-(1, '36983cce-975b-4a92-bf73-a4f41978e01c', 'soumyadips.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'soumyadip', 'ROLE_ADMIN', 'Thu Aug 22 00:00:56 IST 2019'),
-(2, 'ByAdmin-Panel', 'soumyadipp@gmail.com', b'1', 'Soumyadip', 'Male', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:11:47 IST 2019'),
-(4, 'ByAdmin-Panel', 'a.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Wed Jul 03 14:06:52 IST 2019'),
-(6, 'ByAdmin-Panel', 'soumyadip.yoyo@gmail.com', b'1', 'Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_DOCTOR', 'Wed Aug 21 23:59:07 IST 2019'),
-(7, 'ByAdmin-Panel', 'soumydip.cmp@gmail.com', b'1', 'Mr. Soumyadip', 'MALE', 'Chowdhury', 'default', 'ROLE_ADMIN', 'Sat Jun 08 18:25:03 IST 2019'),
-(12, 'a6866ee4-f568-47a9-9a23-2297ec37c293', 'testUser@gmail.com', b'1', 'Soumyadip', 'Male', 'Chowdhury', 'soumyadip', 'ROLE_USER', 'Wed Aug 21 23:57:20 IST 2019'),
-(20, 'ByAdmin-Panel', 's@teamcg.com', b'1', 'Sanket', 'Male', 'Sarkar', 'default', 'ROLE_DOCTOR', 'Fri Jun 14 17:14:51 IST 2019');
+(11, '36983cce-975b-4a92-bf73-a4f41978e01c', 'venkat@gmail.com', b'1', 'ebhuthi', 'MALE', 'nani', 'password', 'ROLE_ADMIN', 'Thu Aug 22 00:00:56 IST 2024'),
+(12, 'ByAdmin-Panel', 'venkat1@gmail.com', b'1', 'venkat', 'Male', 'ebhuthi', 'password', 'ROLE_DOCTOR', 'Fri Jun 04 17:11:47 IST 2024'),
+(13, 'ByAdmin-Panel', 'venkat2@gmail.com', b'1', 'venkat', 'MALE', 'ebhuthi', 'password', 'ROLE_DOCTOR', 'Wed Jul 23 14:06:52 IST 2024'),
+(15, 'ByAdmin-Panel', 'venkat3@gmail.com', b'1', 'venkat', 'MALE', 'ebhuthi', 'password', 'ROLE_DOCTOR', 'Wed Jul 25 23:59:07 IST 2024'),
+(17, 'ByAdmin-Panel', 'venkat4@gmail.com', b'1', 'venkat', 'MALE', 'ebhuthi', 'password', 'ROLE_ADMIN', 'Sat Jun 08 18:25:03 IST 2024'),
+(22, 'a6866ee4-f568-47a9-9a23-2297ec37c293', 'sai@gmail.com', b'1', 'sai', 'Male', 'ebhuthi', 'password', 'ROLE_USER', 'Wed Jun 21 23:57:20 IST 2024'),
+(20, 'ByAdmin-Panel', 'krishna@gmail.com', b'1', 'Sanket', 'Male', 'krishna', 'ebhuthi', 'ROLE_DOCTOR', 'Fri Jun 14 17:14:51 IST 2024');
 
 --
--- Indexes for dumped tables
---
 
---
--- Indexes for table `app`
---
 ALTER TABLE `app`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `app`
---
 ALTER TABLE `app`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
